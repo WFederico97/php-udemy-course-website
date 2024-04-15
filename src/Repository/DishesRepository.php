07614ea612fc15a693cd61b150777fb5b531e696
wfederico97 <wuthrich09@gmail.com>
2024-04-15 15:03:58 -0300

    first commit

---
+++ src/Repository/DishesRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Dishes;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Dishes>
+ *
+ * @method Dishes|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Dishes|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Dishes[]    findAll()
+ * @method Dishes[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class DishesRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Dishes::class);
+    }
+
+//    /**
+//     * @return Dishes[] Returns an array of Dishes objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('d.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Dishes
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
