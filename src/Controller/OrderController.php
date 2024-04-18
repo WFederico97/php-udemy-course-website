77b50655831b520408da83c0fdcdafa00f4b8d78
wfederico97 <wuthrich09@gmail.com>
2024-04-18 01:35:37 -0300

    sec 5 - 32 done

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Dishes;
+use App\Entity\Orders;
+use App\Repository\OrdersRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    #[Route('/orders', name: 'app_orders')]
+    public function index(OrdersRepository $or): Response
     {
+        $orders = $or->findBy(
+            ['counter' => 'Counter 1']
+        );
+        // dump($orders);
         return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+            'orders' => $orders
         ]);
+    }
+    #[Route('/order/{id}', name: 'app_order')]
+    public function order(ManagerRegistry $doctrine ,Dishes $dish){
+        $orders = new Orders();
+        $orders->setCounter('Counter 1');
+        $orders->setName($dish->getName());
+        $orders->setOrderNumber($dish->getId());
+        $orders->setPrice($dish->getPrice());
+        $orders->setStatus('open');
+
+        $em = $doctrine->getManager();
+        $em -> persist($orders);
+        $em -> flush();
+
+        $this->addFlash('orders', $orders->getName(). ' was added to the order');
+
+        return $this->redirect($this->generateUrl('app_menu'));
+    }
+    #[Route('/status/{id}, {status}', name: 'app_status')]
+    public function status(OrdersRepository $or,ManagerRegistry $doctrine ,$id,$status){
+        $em = $doctrine->getManager();
+        $order = $or->find($id) ;
+        $order->setStatus($status);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('app_orders'));
+    }
+    #[Route('/delete/{id}', name: 'app_order_delete')]
+    public function deleteOrder(ManagerRegistry $doctrine, OrdersRepository $or, int $id)
+    {
+
+        $em = $doctrine->getManager();
+        $order = $or->find($id); //find trae el objeto por id & findAll te trae un array con los objetos de la identidad
+        $em->remove($order);
+        $em->flush();
+
+        $this->addFlash('success', 'Order removed successfully');
+        return $this->redirect($this->generateUrl('app_orders'));
     }
 }
--- src/Entity/Orders.php
+++ src/Entity/Orders.php
@@ -16,6 +16,18 @@ class Orders
     #[ORM\Column(length: 255)]
     private ?string $counter = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $oNumber = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $status = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +41,54 @@ class Orders
     public function setCounter(string $counter): static
     {
         $this->counter = $counter;
+
+        return $this;
+    }
+
+    public function getOrderNumber(): ?string
+    {
+        return $this->oNumber;
+    }
+
+    public function setOrderNumber(?string $order_number): static
+    {
+        $this->oNumber = $order_number;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): static
+    {
+        $this->status = $status;
 
         return $this;
     }
